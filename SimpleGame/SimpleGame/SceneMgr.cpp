ad65c7276910f6c6b03a2dd90e05e59db6df58f7
miro <druid0228@naver.com>
2017-10-19 13:45:56 +0900

    clean

---
+++ SimpleGame/SimpleGame/SceneMgr.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "SceneMgr.h"
+
+
+SceneMgr::SceneMgr()
+{
+}
+
+
+SceneMgr::~SceneMgr()
+{
+}
---
+++ SimpleGame/SimpleGame/SceneMgr.h
@@ -0,0 +1,8 @@
+#pragma once
+class SceneMgr
+{
+public:
+	SceneMgr();
+	~SceneMgr();
+};
+
